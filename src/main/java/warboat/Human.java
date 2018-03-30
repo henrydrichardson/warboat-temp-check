6456e94e6d686397efabdaf9a3cbf7ef06fc7b60
varnikakuthuru <31871461+varnikakuthuru@users.noreply.github.com>
2018-03-29 20:48:48 -0400

    Update Human.java

--- src/main/java/warboat/Human.java
+++ src/main/java/warboat/Human.java
@@ -1,15 +1,19 @@
 package warboat;
 
 public class Human {
-  
+
   private int id;
+  private String gamerTag;
   private int wins;
   private int losses;
   private int score;
   private int currency;
+  private ArrayList<Item> purchasedItems;
+
 
   public Human(int id) {
     this.id = id;
+    this.gamerTag = "johnSnow";
     this.wins = 10;
     this.losses = 20;
     this.score = 30;
@@ -29,9 +33,29 @@ public class Human {
   }
 
   public int getScore() {
-    return score;
+    return (wins/losses);
   }
+
   public int getCurrency() {
-    return currency; 
+    return currency;
   }
+
+  public ArrayList<Integer> getPurchased() {
+    return { Item(1), Item(2), Item(4) };
+  }
+
+  public void setWins(int newValue) {
+    this.wins = newValue;
+  }
+
+  public void setLosses(int newValue) {
+    this.losses = newValue;
+  }
+
+  public void setCurrency(int newValue) {
+    this.currency = newValue;
+  }
+
+
+
 }
