af3b95f3169e935321d4b4f8040b2694e5507b95
Varnika <varnikakuthuru1@gmail.com>
2018-04-13 23:31:21 -0400

    game play

--- Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/GamePlay.java
+++ Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/GamePlay.java
@@ -1,8 +1,22 @@
 package com.example.samanthamorris.warboat;
 
-public class GamePlay {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.util.Log;
+
+public class GamePlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_grid);
 
 
+    }
     public GamePlay(Player One, Player Two)
     {
         //Populate Boats
