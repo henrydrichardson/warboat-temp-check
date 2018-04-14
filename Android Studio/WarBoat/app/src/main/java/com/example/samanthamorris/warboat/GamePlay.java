14f6a5431960bf9dce6c11c80a5ff7e7afe08b41
Varnika <varnikakuthuru1@gmail.com>
2018-04-14 13:26:40 -0400

    Grid buttons attempt

--- Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/GamePlay.java
+++ Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/GamePlay.java
@@ -8,28 +8,36 @@ import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.util.Log;
+import android.view.View;
 
 public class GamePlay extends AppCompatActivity {
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_grid);
+
+
+    }
 
     public Grid[] GridManager = new Grid[2];
 
     public GamePlay(Player One, Player Two)
     {
+
+
+//        GridLayout mainGrid = findViewById(R.id.mainGrid);
+//        int childCount = mainGrid.getChildrenCount();
+//
+//        for(int i = 0; i < childCount; i++){
+//            RelativeLayout container = (RelativeLayout) mainGrid.getChildAt(i);
+//            container.setOnClickListener(new View.OnClickListener(){
+//                public void onClick(View view){
+//                    // do stuff
+//                }
+//            });
+//        }
+
         // TODO: Populate Boats
-
-
-        GridLayout mainGrid = findViewById(R.id.mainGrid);
-        int childCount = mainGrid.getChildrenCount();
-
-        for(int i = 0; i < childCount; i++){
-            RelativeLayout container = (RelativeLayout) mainGrid.getChildAt(i);
-            container.setOnClickListener(new View.OnClickListener(){
-                public void onClick(View view){
-                    // do stuff
-                }
-            });
-        }
 
         // Turn Manager
         int turn = 0;
@@ -38,13 +46,13 @@ public class GamePlay extends AppCompatActivity {
         while (!gameIsOver)
         {
             // Display other player's grid
-            GridManager[(turn+1)%2].display();
-            GridManager[turn%2].player.attack();
-
-            // If attack causes a game loss for the other player, the game is over
-            if (GridManager[(turn+1)%2].isLost())
-                gameIsOver = false;
-            turn++;
+//            GridManager[(turn+1)%2].display();
+//            GridManager[turn%2].player.attack();
+//
+//            // If attack causes a game loss for the other player, the game is over
+//            if (GridManager[(turn+1)%2].isLost())
+//                gameIsOver = false;
+//            turn++;
         }
 
 
--- Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/Grid.java
+++ Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/Grid.java
@@ -2,6 +2,10 @@ package com.example.samanthamorris.warboat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Button;
+
 import java.util.ArrayList;
 
 public class Grid extends AppCompatActivity {
@@ -16,16 +20,39 @@ public class Grid extends AppCompatActivity {
     private ArrayList<Integer> SHIP_POINTS = new ArrayList<Integer>();
     private ArrayList<Integer> ATTACK_POINTS = new ArrayList<Integer>();
 
+    public int[] ButtonIds = {R.id.b1, R.id.b2, R.id.b3, R.id.b4, R.id.b5, R.id.b6, R.id.b7, R.id.b8,
+                                R.id.b9, R.id.b10, R.id.b11, R.id.b12, R.id.b13, R.id.b14, R.id.b15, R.id.b16,
+                                R.id.b17, R.id.b18, R.id.b19, R.id.b20, R.id.b21, R.id.b22, R.id.b23, R.id.b24,
+                                R.id.b25, R.id.b26, R.id.b27, R.id.b28, R.id.b29, R.id.b30, R.id.b31, R.id.b32,
+                                R.id.b33, R.id.b34, R.id.b35, R.id.b36, R.id.b37, R.id.b38, R.id.b39, R.id.b40,
+                                R.id.b41, R.id.b42, R.id.b43, R.id.b44, R.id.b45, R.id.b46, R.id.b47, R.id.b48,
+                                R.id.b49, R.id.b50, R.id.b51, R.id.b52, R.id.b53, R.id.b54, R.id.b55, R.id.b56,
+                                R.id.b57, R.id.b58, R.id.b59, R.id.b60, R.id.b61, R.id.b62, R.id.b63, R.id.b64
+                                };
+    public Button[] GridButtons = new Button[ButtonIds.length+1];
+
+
 
     public Grid(Player Current)
     {
+        for (int i = 0; i <= ButtonIds.length; i++) {
+            GridButtons[i + 1] = (Button) findViewById(ButtonIds[i]);
+
+            GridButtons[i + 1].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
 
     }
 
-//    public boolean isVisible(Player Current)
-//    {
-//
-//    }
+    public boolean isVisible(Player Current)
+    {
+        // TODO
+        return true;
+    }
 
     public void setAttackPoints(int buttonIndex)
     {
@@ -71,14 +98,15 @@ public class Grid extends AppCompatActivity {
                 }
             }
             return Misses;
-        }
+
     }
 
-//
-//    public boolean isShipVisible(Player Current)
-//    {
-//
-//    }
+
+    public boolean isShipVisible(Player Current)
+    {
+        // TODO
+        return true;
+    }
 
     // Checks if the current Grid is lost
     public boolean isGameLost()
