cb1a8f6ae5025af9c39cc24fb756ce94a328aa13
Varnika <varnikakuthuru1@gmail.com>
2018-04-14 02:30:08 -0400

    Ship- setLocation and Rotate functionality

--- Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/Ship.java
+++ Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/Ship.java
@@ -3,6 +3,7 @@ package com.example.samanthamorris.warboat;
 public class Ship {
 
     public int length;
+    public boolean isVertical = false;
 
     public Ship()
     {
@@ -27,10 +28,29 @@ public class Ship {
 //    }
 //    public void setPosition()
 //    {
+//        // Prompt user for starting location
+//        // Use Actionlistner which index is located
+//        //
+//        //
+//        if (isVertical==true)
+//        {
+//            for (int i = 0; i < length; i++)
+//            {
+//               //Player Grid Ship Location and add head+i
+//            }
+//        }
+//        if (isVertical==false)
+//        {
+//            for (int i = 0; i < length; i++)
+//            {
+//                //Player Grid Ship Location and add head + (i*8)
+//            }
+//        }
 //
 //    }
 //    public void rotate()
 //    {
-//
+//        isVertical = true;
 //    }
+
 }
