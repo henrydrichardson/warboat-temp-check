5ff068017ed28f04c0bdee5a228202a5c5ce3372
U-LAPTOP-F66O314U\jourd <mrflibbles914@hotmail.com>
2018-04-18 18:48:41 -0400

    eturn to home at the end of the game

--- Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/LostGame.java
+++ Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/LostGame.java
@@ -3,11 +3,23 @@ package com.example.samanthamorris.warboat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LostGame extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_lost);
+
+        ImageButton back = (ImageButton) findViewById(R.id.lost_to_home);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(LostGame.this,
+                        StartupScreen.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
