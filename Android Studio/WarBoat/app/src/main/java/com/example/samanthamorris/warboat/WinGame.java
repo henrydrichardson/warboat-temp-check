89577a14479fd4b41b4bea90bb6b8e87f38063d1
U-LAPTOP-F66O314U\jourd <mrflibbles914@hotmail.com>
2018-04-18 18:48:41 -0400

    eturn to home at the end of the game

--- Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/WinGame.java
+++ Android Studio/WarBoat/app/src/main/java/com/example/samanthamorris/warboat/WinGame.java
@@ -1,7 +1,10 @@
 package com.example.samanthamorris.warboat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class WinGame extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class WinGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_won);
+
+        ImageButton back = (ImageButton) findViewById(R.id.won_to_home);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(WinGame.this,
+                        StartupScreen.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
