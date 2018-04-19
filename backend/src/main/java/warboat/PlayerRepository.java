ff971a19a5cf87058aaeedb4bc24ff7a3bfafcad
Cecilie <ceciliebarreto@gmail.com>
2018-04-18 23:13:05 -0400

    Working oauth login that generates player database entry

--- backend/src/main/java/warboat/PlayerRepository.java
+++ backend/src/main/java/warboat/PlayerRepository.java
@@ -1,10 +1,13 @@
 package warboat;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 import warboat.Player;
 
 // Auto built by Spring using special bean tech. NO TOUCHY
 
 public interface PlayerRepository extends CrudRepository<Player, Long> {
+
+    List<Player> findByEmail(String email);
 }
